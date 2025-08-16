1639485eaf27e905cca424a0450e2db15200a17c
bhoomigundecha <bhoomigundecha@gmail.com>
2025-08-16 19:56:51 +0530

    Added api routes

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,21 +1,14 @@
 'use client';
-import '@/assets/styles/global.css';
-
-import Link from 'next/link';
-import { useRouter, useParams, useSearchParams, usePathname } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { useParams } from 'next/navigation';
 
 const PropertiesPage = () => {
-    const router = useRouter();
-    const { id } = useParams();
-    const searchParams = useSearchParams();
-    const name = searchParams.get('name');
+    
     return (
-        <div>
-            <button onClick={() => router.push('/')} className="bg-blue-500 p-2">
-                Go Home
-            </button>
-        </div>
-    );
+        <>
+        
+        </>
+    )
 }
 
 export default PropertiesPage;
--- utils/requests.js
+++ utils/requests.js
@@ -1,5 +1,6 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
+// fetch all properties
 async function fetchProperties() {
   try {
     // Handle the case where the domain is not available yet 
@@ -22,4 +23,28 @@ async function fetchProperties() {
   }
 }
 
-export { fetchProperties };
+// Fetch single property 
+async function fetchProperty(id) {
+  try {
+    // Handle the case where the domain is not available yet 
+
+    if(!apiDomain){
+        return null;
+    }
+
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/properties/${id}`);
+
+    if (!res.ok) {
+      console.error("Fetch failed with status:", res.status);
+      return null; 
+    }
+
+    return await res.json();
+  } catch (error) {
+    console.error("fetchProperties error:", error);
+    return null; 
+  }
+}
+
+
+export { fetchProperties, fetchProperty };  // Export the functions to be used elsewhere in the app
