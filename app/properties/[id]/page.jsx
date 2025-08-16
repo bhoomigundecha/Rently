a4d17f31eafa43b69d9cc3e71e86b24feb8ca833
bhoomigundecha <bhoomigundecha@gmail.com>
2025-08-16 20:22:32 +0530

    Single property fetch

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,12 +1,39 @@
 'use client';
 import { useEffect, useState } from 'react';
 import { useParams } from 'next/navigation';
+import { fetchProperty } from '@/utils/requests';
 
-const PropertiesPage = () => {
+
+const PropertyPage = () => {
+    const { id } = useParams();
+
+    const [property, setProperty] = useState(null);
+
+    const [loading, setLoading] = useState(true);
+
+    useEffect(()=>{
+        const fetchProperty = async () => {
+            if(!id) return;
+            try{    
+                const property = await fetchProperty(id);
+                setProperty(property);
+
+            }catch(err){
+                console.error(err);
+                console.error("Error catching prop : ", err);
+            }finally{
+                setLoading(false);
+            }
+        }
+
+        if(property===null){
+            fetchProperty(id);
+        }
+    }, [id, property]);
     
     return (
         <>
-        
+
         </>
     )
 }
