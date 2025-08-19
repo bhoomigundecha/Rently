be9c441f3c50a22e9bce9be997a619776778e268
bhoomigundecha <bhoomigundecha@gmail.com>
2025-08-19 19:41:29 +0530

    Added post api route to save property to the database

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -16,7 +16,47 @@ export const GET = async(request) => {
 } 
 
 export const POST = async(request) => {
+
+
   try{
+    const FormData = await request.formData();
+
+    // Access all values from amenties and images 
+
+    const amenties = FormData.getAll('amenties')
+    const images = FormData.getAll('images').filter((image)=>image.name!=='');
+
+    // Create propertyData object for the database 
+
+    const propertyData = {
+      type : FormData.get('type'),
+      name : FormData.get('name'),
+      description : FormData.get('description'),
+      location : {
+        street : FormData.get('location.street'),
+        city : FormData.get('location.city'),
+        state : FormData.get('location.state'),
+        zipcode : FormData.get('location.zipcode'),
+      },
+      beds : FormData.get('beds'),
+      baths : FormData.get('baths'),
+      square_feet : FormData.get('square_feet'),
+      amenties, 
+      rates : {
+        weekly : FormData.get('location.weekly'),
+        monthly : FormData.get('location.monthly'),
+        nightly : FormData.get('location.nightly'),
+      },
+      seller_info : {
+        name : FormData.get('seller_info.name'),
+        email : FormData.get('seller_info.email'),
+        phone : FormData.get('seller_info.phone'),
+      },
+      images
+    }
+
+    console.log(propertyData)
+
     return new Response(JSON.stringify({message : 'Success'}), {status : 200})
   }catch(err){
     return new Response(JSON.stringify({message : 'Failed to add property'}), {status : 500})
